6eb313b89620348a0e054a62a1b68e6a3bd19fa5
DoakesdextermothaF <1217945@student.roc-nijmegen.nl>
2025-11-20 12:15:06 +0100

    update
    eod

--- arrays.java
+++ arrays.java
@@ -6,18 +6,32 @@ public class arrays {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> evenGetallen = new ArrayList<>();
+    	//opdracht 1
+        ArrayList<Integer> evenGetallen = new ArrayList<Integer>();
 
         for (int i = 0; i < 100; i++) {
             if (i % 2 == 0) {
                 evenGetallen.add(i);
+                
+                
             }
         }
 
-        System.out.println(evenGetallen);
         
-       for (int i = 2; i < 50; i++) {
+        //opdracht 2
+        ArrayList<Long> evenGetallenreeks = new ArrayList<Long>();
+        evenGetallenreeks.add(0L);
+        evenGetallenreeks.add(1L);
+        
+       for (int i = 2; i < 50; i++) 
+       {
+    	   
+    	   long lastValue = evenGetallenreeks.get(evenGetallenreeks.size() -1);
+    	   long secondeToLastValue = evenGetallenreeks.get(evenGetallenreeks.size() -2);
+    	   long som = lastValue + secondeToLastValue;
+    	   evenGetallenreeks.add(som);
     	   }
+       
        }
         
     }
